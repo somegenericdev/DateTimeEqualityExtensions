4728b39e3b266181a139e5e289c77ea590e24819
somegenericdev <95114021+somegenericdev@users.noreply.github.com>
2024-05-04 21:13:53 +0200

    first commit

---
+++ DateTimeEqualityExtensions/DateTimeEqualityExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace DateTimeEqualityExtensions;
+
+public static class DateTimeEqualityExtensions
+{
+    public static bool IsGreater(this DateTime? dt1, DateTime? dt2)
+    {
+        return Nullable.Compare(dt1, dt2) > 0;
+    }
+
+    public static bool IsGreaterOrEqual(this DateTime? dt1, DateTime? dt2)
+    {
+        return Nullable.Compare(dt1, dt2) >= 0;
+    }
+    public static bool IsLess(this DateTime? dt1, DateTime? dt2)
+    {
+        return Nullable.Compare(dt1, dt2) < 0;
+    }
+
+    public static bool IsLessOrEqual(this DateTime? dt1, DateTime? dt2)
+    {
+        return Nullable.Compare(dt1, dt2) <= 0;
+    }
+}
